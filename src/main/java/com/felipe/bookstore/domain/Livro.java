f88db8bfda4c96d3a7c23f004b23d24e2bf72ee5
feliperb12 <feliperibeiroaraujo3@hotmail.com>
2022-07-28 19:54:33 -0300

    Implementando as classes de domínio

--- src/main/java/com/felipe/bookstore/domain/Livro.java
+++ src/main/java/com/felipe/bookstore/domain/Livro.java
@@ -1,6 +1,14 @@
 package com.felipe.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,15 +17,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Livro {
+@Entity
+public class Livro implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String Titulo;
 	private String nome_autor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
-
-	
 
 }
