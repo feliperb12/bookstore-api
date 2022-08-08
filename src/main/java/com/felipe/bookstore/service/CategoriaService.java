56a71a677de23df5f687c376d1d2a5f4c484d55d
feliperb12 <feliperibeiroaraujo3@hotmail.com>
2022-08-08 10:05:56 -0300

    Excessão personalizada para objeto não encontrada

--- src/main/java/com/felipe/bookstore/service/CategoriaService.java
+++ src/main/java/com/felipe/bookstore/service/CategoriaService.java
@@ -6,18 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipe.bookstore.domain.Categoria;
+import com.felipe.bookstore.exception.ObjectNotFoundException;
 import com.felipe.bookstore.repositories.CategoriaRespository;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRespository respository;
-	
-	
+
 	public Categoria findyById(Integer id) {
-	 	Optional<Categoria> obj = respository.findById(id);
-	 	return obj.orElse(null);
+		Optional<Categoria> obj = respository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado!! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
